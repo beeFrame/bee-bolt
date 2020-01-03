12cba3c958529f90f50d9502d1aee8206bb89265
jh <zong.jianhui06@gmail.com>
2020-01-03 14:24:56 +0800

    rename readme

---
+++ src/main/java/com/github/beeFrame/remoting/BeeBoltApplication.java
@@ -0,0 +1,13 @@
+package com.github.beeFrame.remoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeBoltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeeBoltApplication.class, args);
+	}
+
+}
